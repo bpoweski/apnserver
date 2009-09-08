792e8320cb4061863e5d37f091c10779d988f8a1
Ben Poweski <bpoweski@gmail.com>
2009-09-08 11:05:57 -0500

    removed unused test helper method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,2 @@
 require 'rubygems'
 require 'test/unit'
-
-def symbolize_keys(hash)  
-  hash.inject({}) do |result, (key, value)|  
-    new_key = case key  
-      when String then key.to_sym  
-    else key  
-    end  
-    new_value = case value  
-      when Hash then symbolize_keys(value)  
-    else value  
-    end  
-    result[new_key] = new_value  
-    result  
-  end
-end  
