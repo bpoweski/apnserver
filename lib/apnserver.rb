9648872c7a73c8750cb3534895ba7bc0f71055d6
Ben Poweski <bpoweski@gmail.com>
2009-09-12 21:17:48 -0500

    added VERSION and gemspec

--- lib/apnserver.rb
+++ lib/apnserver.rb
@@ -1,11 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
   
-
-module Apnserver
-  VERSION = '0.0.1'
-end
-
 require 'rubygems'
 gem 'eventmachine', '>= 0.12.8'
 require 'eventmachine'
