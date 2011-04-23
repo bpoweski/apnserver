056a691c767059ae350124030eedc648002193a6
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-04-23 16:18:57 -0400

    Removed stale .swp, now closing sockets after 18 hours, apple doesn't like long opened sockets.

--- lib/racoon/server.rb
+++ lib/racoon/server.rb
@@ -11,36 +11,42 @@ module Racoon
       @beanstalkd_uris = beanstalkd_uris
     end
 
-    def beanstalk
-      return @@beanstalk if @@beanstalk
-      @@beanstalk = Beanstalk::Pool.new @beanstalkd_uris
-      %w{watch use}.each { |s| @@beanstalk.send(s, 'racoon') }
-      @@beanstalk.ignore('default')
-      @@beanstalk
+    def beanstalk(arg)
+      return @beanstalks[arg] if @beanstalks[arg]
+      @beanstalks[arg] = Beanstalk::Pool.new @beanstalkd_uris
+      %w{watch use}.each { |s| @beanstalks[arg].send(s, "racoon-#{arg}") }
+      @beanstalks[arg].ignore('default')
+      @beanstalks[arg]
     end
 
     def start!
       EventMachine::run do
-        EventMachine::PeriodicTimer.new(28800) do
+        EventMachine::PeriodicTimer.new(3600) do
           begin
-            @feedback_client = nil # Until we pull in DB support
-            @feedback_client.connect! unless @feedback_client.connected?
-            @feedback_client.read.each do |record|
-              feedback_callback.call record
+            if beanstalk('feedback').peek_ready
+              item = beanstalk('feedback').reserve(1)
+              handle_feedback(item)
             end
-            @feedback_client.disconnect!
-          rescue Errno::EPIPE, OpenSSL::SSL::SSLError, Errno::ECONNRESET
-            Config.logger.error "(Feedback) Caught Error, closing connection"
-            @feedback_client.disconnect!
-          rescue RuntimeError => e
-            Config.logger.error "(Feedback) Unable to handle: #{e}"
+          rescue Beanstalk::TimedOut
+            Config.logger.info "(Beanstalkd) Unable to secure a job, operation timed out."
+          end
+        end
+
+        EventMachine::PeriodicTimer.new(60) do
+          begin
+            if beanstalk('killer').peek_ready
+              item = beanstalk('killer').reserve(1)
+              purge_client(item)
+            end
+          rescue Beanstalk::TimedOut
+            Config.logger.info "(Beanstalkd) Unable to secure a job, operation timed out."
           end
         end
 
         EventMachine::PeriodicTimer.new(1) do
           begin
-            if beanstalk.peek_ready
-              item = beanstalk.reserve(1)
+            if beanstalk('apns').peek_ready
+              item = beanstalk('apns').reserve(1)
               handle_job item
             end
           rescue Beanstalk::TimedOut
@@ -62,7 +68,7 @@ module Racoon
       packet = job.ybody
       project = packet[:project]
       if notification = Notification.new.create_payload(packet[:notification])
-        client = get_client(project.name, project.certificate, packet[:sandbox])
+        client = get_client(project[:name], project[:certificate], packet[:sandbox])
         begin
           client.connect! unless client.connected?
           client.write(notification)
@@ -89,8 +95,37 @@ module Racoon
       end
     end
 
+    # Will be a hash with two keys:
+    # :certificate and :sandbox.
+    def handle_feedback(job)
+      begin
+        packet = job.ybody
+        uri = "feedback.#{packet[:sandbox] ? 'sandbox.' : ''}push.apple.com"
+        feedback_client = Racoon::FeedbackClient.new(packet[:certificate], uri)
+        feedback_client.connect!
+        feedback_client.read.each do |record|
+          feedback_callback.call record
+        end
+        feedback_client.disconnect!
+        job.delete
+      rescue Errno::EPIPE, OpenSSL::SSL::SSLError, Errno::ECONNRESET
+        Config.logger.error "(Feedback) Caught Error, closing connection"
+        feedback_client.disconnect!
+        job.release
+      rescue RuntimeError => e
+        Config.logger.error "(Feedback) Unable to handle: #{e}"
+        job.delete
+      end
+    end
+
     def get_client(project_name, certificate, sandbox = false)
       uri = "gateway.#{sandbox ? 'sandbox.' : ''}push.apple.com"
+      unless @clients[project_name]
+        @clients[project_name] = Racoon::Client.new(certificate, uri)
+        # in 18 hours (64800 seconds) we need to schedule this socket to be killed. Long opened
+        # sockets don't work.
+        beanstalk('killer').yput({:certificate => certificate, :sandbox => sandbox}, 65536, 64800)
+      end
       @clients[project_name] ||= Racoon::Client.new(certificate, uri)
       client = @clients[project_name]
 
@@ -98,11 +133,19 @@ module Racoon
       # disconnect and reconnect.
       unless client.pem.eql?(certificate)
         client.disconnect! if client.connected?
-        @clients[project_name] = ApnServer::Client.new(certificate, uri)
+        @clients[project_name] = Racoon::Client.new(certificate, uri)
         client = @clients[project_name]
       end
 
       client
+    end
+
+    def purge_client(job)
+      project_name = job.ybody
+      client = @clients[project_name]
+      client.disconnect! if client
+      @clients[project_name] = nil
+      job.delete
     end
   end
 end
