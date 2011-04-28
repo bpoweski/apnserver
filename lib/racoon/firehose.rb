b6aa3841dce85e8b694fe899d7575839cb2b4361
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-04-28 02:13:58 -0400

    added firehose and worker -- first draft, probably doesn't work right.
    Also added associated documentation

---
+++ lib/racoon/firehose.rb
@@ -0,0 +1,58 @@
+# Racoon - A distributed APNs provider
+# Copyright (c) 2011, Jeremy Tregunna, All Rights Reserved.
+#
+# This module contains the firehose which is responsible for maintaining all the open
+# connections to Apple, and sending data over the right ones.
+
+require 'digest/sha1'
+
+module Racoon
+  class Firehose
+    def initialize(context = ZMQ::Context.new(1), address = "tcp://*:11555")
+      @connections = {}
+      @context = context
+      @firehose = context.socket(ZMQ::PULL)
+      @firehose.bind(address)
+    end
+
+    def start!
+      EventMachine::run do
+        apns = EventMachine.spawn do |project, bytes|
+          uri = "gateway.#{project[:sandbox] ? 'sandbox.' : ''}push.apple.com"
+          hash = project_hash(project)
+
+          should_fail_in_exception_handler = false
+
+          begin
+            @connection[hash] ||= Racoon::Client.new(project[:certificate], uri)
+
+            @connection[hash].connect! unless @connection[hash].connected?
+            @connection[hash].write(bytes)
+          rescue Errno::EPIPE, OpenSSL::SSL::SSLError, Errno::ECONNRESET
+            @connection[hash].disconnect!
+            retry unless should_fail_in_exception_handler
+            should_fail_in_exception_handler = true
+          end
+        end
+
+        EventMachine::PeriodicTimer.new(0.1) do
+          received_message = ZMQ::Message.new
+          @firehose.recv(received_message, ZMQ::NOBLOCK)
+          json_string = received_message.copy_out_string
+          
+          if json_string
+            packet = Yajl::Parser.parse(json_string)
+
+            apns.notify(packet[:project], packet[:bytes])
+          end
+        end
+      end
+    end
+
+    private
+
+    def project_hash(project)
+      Digest::SHA1.hexdigest("#{project[:name]}-#{project[:certificate]}")
+    end
+  end
+end
---
+++ lib/racoon/worker.rb
@@ -0,0 +1,68 @@
+# Racoon - A distributed APNs provider
+# Copyright (c) 2011, Jeremy Tregunna, All Rights Reserved.
+#
+# This module contains the worker which processes notifications before sending them off
+# down to the firehose.
+
+module Racoon
+  class Worker
+    def initialize(beanstalk_uris, context = ZMQ::Context.new(1), address = "tcp://*:11555")
+      @beanstalk_uris = beanstalk_uris
+      @context = context
+      @firehose = context.socket(ZMQ::PUSH)
+      @firehose.connect(address)
+      # First packet, send something silly, the firehose ignores it
+      @send_batch = true
+    end
+    
+    def start!
+      EventMachine::run do
+        if @send_batch
+          @send_batch = false
+          @firehose.send_string("")
+        end
+
+        EventMachine::PeriodicTimer.new(0.5) do
+          begin
+            if beanstalk.peek_ready
+              job = beanstalk.reserve(1)
+              process job
+            end
+          rescue Beanstalk::TimedOut
+            Config.logger.info "[Beanstalk] Unable to secure job, operation timed out."
+          end
+        end
+      end
+    end
+
+    private
+
+    def beanstalk
+      return @beanstalk if @beanstalk
+      @beanstalk ||= Beanstalk::Pool.new(@beanstalk_uris)
+      %w{use watch}.each { |s| @beanstalk.send(s, 'racoon') }
+      @beanstalk.ignore('default')
+      @beanstalk
+    end
+
+    # Expects json ala:
+    # json = {
+    #   "project":{
+    #      "name":"foobar",
+    #      "certificate":"...",
+    #      "sandbox":false
+    #   },
+    #   "bytes":"..."
+    # }
+    def process(job)
+      json_string = job.body
+      packet = Yajl::Parser.parse(json_string)
+      project = packet[:project]
+
+      notification = Notification.create_from_packet(packet)
+
+      data = { :project => project, :bytes => notification.to_bytes }
+      @firehose.send_string(Yajl::Encoder.encode(data))
+    end
+  end
+end
